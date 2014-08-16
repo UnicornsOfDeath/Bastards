31c49f97fec141cbc0013f61b3c7df502c32ee0b
Cong <congusbongus@gmail.com>
2014-08-16 19:13:20 +1000

    Add walking pandas

--- scripts/room_player.js
+++ scripts/room_player.js
@@ -1,10 +1,11 @@
 var ROOM_PLAYER_SPEED = 200;
 var RoomPlayer = function(game, x, y) {
   Phaser.Sprite.call(this, game, x, y, 'panda');
-  this.animations.add('down', [0, 1, 2], 4);
-  this.animations.add('up', [3, 4, 5], 4);
-  this.animations.add('right', [6, 7, 8], 4);
-  this.animations.add('left', [9, 10, 11], 4);
+  var frameRate = 10;
+  this.animations.add('down', [0, 1, 2], frameRate);
+  this.animations.add('up', [3, 4, 5], frameRate);
+  this.animations.add('right', [6, 7, 8], frameRate);
+  this.animations.add('left', [9, 10, 11], frameRate);
 
   game.physics.arcade.enable(this);
 
@@ -15,3 +16,26 @@ var RoomPlayer = function(game, x, y) {
 };
 RoomPlayer.prototype = Object.create(Phaser.Sprite.prototype);
 RoomPlayer.prototype.constructor = RoomPlayer;
+
+RoomPlayer.prototype.update = function() {
+  // find the dominant direction
+  var dominantDir = null;
+  var fastestSpeed = 0;
+  if (this.body.velocity.x > fastestSpeed) {
+    dominantDir = 'right';
+    fastestSpeed = this.body.velocity.x;
+  }
+  if (-this.body.velocity.x > fastestSpeed) {
+    dominantDir = 'left';
+    fastestSpeed = -this.body.velocity.x;
+  }
+  if (this.body.velocity.y > fastestSpeed) {
+    dominantDir = 'down';
+    fastestSpeed = this.body.velocity.y;
+  }
+  if (-this.body.velocity.y > fastestSpeed) {
+    dominantDir = 'up';
+    fastestSpeed = -this.body.velocity.y;
+  }
+  this.animations.play(dominantDir);
+};
